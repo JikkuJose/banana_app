e9f8a5a97e662211a87bfe278588db03d8688cce
Jikku Jose <jikkujose@gmail.com>
2015-11-01 19:18:24 +0530

    Rebase dropdown fix commits
    Update script to invoke dropdown
    
    Update layout to invoke dropdown

--- bower_components/semantic/dist/script.js
+++ bower_components/semantic/dist/script.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
-  $('.item.right').popup({
-    inline: true,
-    on: 'click',
+  $('.title').click(function() {
+    alert('That worked!')
+  });
+
+  $('.name_card').popup({
+    popup: '.drop_down'
+  });
+
+  $('.dropdown').dropdown();
+
+  $('.log_out').click(function(){
+    window.location = '/log_out';
   });
 });
